b71b250a480ca3bc2767c36ebc5b717fbe98f3b4
cmhayden <callumhayden.com>
2024-09-18 22:04:17 +0100

    Setup postgress database with docker

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import { env } from "@/env/server";
+
+const queryClient = postgres(env.DATABASE_URL);
+const db = drizzle(queryClient);
+export default db;
--- src/env/server.ts
+++ src/env/server.ts
@@ -8,6 +8,12 @@ export const env = createEnv({
     NEXTAUTH_SECRET: z.string(),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    DB_HOST: z.string(),
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_NAME: z.string(),
+    DB_PORT: z.coerce.number(),
+    DATABASE_URL: z.string().url(),
   },
   onValidationError: (error: ZodError) => {
     console.error(
