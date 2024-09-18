26e7a8d71d493596b021bb8b83bc77dc80e9b5d2
cmhayden <callumhayden.com>
2024-09-18 14:35:13 +0100

    Make basic landing page

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -9,12 +9,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export default function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider attribute="class">
-        {children}
-      </NextThemesProvider>
+    <NextUIProvider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      navigate={router.push}
+      className="flex h-full w-full flex-col"
+    >
+      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
     </NextUIProvider>
   );
 }
