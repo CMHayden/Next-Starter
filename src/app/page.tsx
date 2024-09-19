53e57e402420d81e5aa127b03564a9234093d290
cmhayden <callumhayden.com>
2024-09-19 11:06:50 +0100

    Fix middleware issue with JWT token vs session table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import { Card, CardBody } from "@nextui-org/react";
-import { sql } from "drizzle-orm";
-
-import db from "@/db";
 
 export default async function Home() {
-  const result = await db.execute(sql`
-    SELECT * FROM pg_catalog.pg_tables
-  `);
   return (
     <Card className="mx-auto mt-4 max-w-md">
       <CardBody className="text-center">
         <h1 className="text-5xl">Next.js Starter</h1>
         <p className="text-xl">A simple starter for Next.js</p>
-        <pre>{JSON.stringify(result, null, 2)}</pre>
       </CardBody>
     </Card>
   );
--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,12 +1,20 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import db from "@/db";
 import { env } from "@/env/server";
 
 const options: NextAuthOptions = {
+  // Can change session strategy to not use DB if use of middleware.ts file is needed
+  // session: {
+  //   strategy: "jwt",
+  // },
   pages: {
     signIn: "/",
   },
+  // @ts-expect-error: Drizzle Adapter is for next auth v5 but we're using v4
+  adapter: DrizzleAdapter(db),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
